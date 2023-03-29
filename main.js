19367b8c2de480f0b1c8ac9469e2d9f0677cb582
André de Oliveira <103377724+Andreprado20@users.noreply.github.com>
2023-03-29 12:14:21 -0300

    Update main.js

--- main.js
+++ main.js
@@ -64,6 +64,13 @@ const user = {
 };
 
 
+function togglePassword(){
+  document.querySelectorAll(".eye").forEach((eye) => eye.classList.toggle("hide"))
+
+  const type = senha.getAttribute("type") == "password" ? "text" : "password";
+
+  senha.setAttribute("type", type);
+}
 
 {/*
 //const { name, ...rest } = user
